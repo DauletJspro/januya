aa18fbe3ed1417681f63e12348f5d7db1e08fd49
AminDiyasSerikuly <amindiyass@gmail.com>
2020-08-23 20:14:32 +0600

    hide elements for first release part 5

--- resources/views/admin/new_design_auth/login.blade.php
+++ resources/views/admin/new_design_auth/login.blade.php
@@ -74,8 +74,8 @@
                             <div class="footer">
                                 <div class="form-group" style="text-align: center">
                                     <div class="col-md-6 main-page-div">
-                                        <a style="font-weight: bold; text-decoration: underline; color: black;"
-                                           href="/reset-password">Забыли пароль?</a>
+{{--                                        <a style="font-weight: bold; text-decoration: underline; color: black;"--}}
+{{--                                           href="/reset-password">Забыли пароль?</a>--}}
                                     </div>
                                     <div class="col-md-6 main-page-div">
                                         <a style="font-weight: bold; text-decoration: underline; color: black;"
