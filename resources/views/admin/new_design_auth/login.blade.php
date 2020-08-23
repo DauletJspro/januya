6ea03f49d735e9c3ef4f6311e57948a1209faa1b
AminDiyasSerikuly <amindiyass@gmail.com>
2020-08-23 20:17:20 +0600

    hide elements for first release part 6

--- resources/views/admin/new_design_auth/login.blade.php
+++ resources/views/admin/new_design_auth/login.blade.php
@@ -4,7 +4,7 @@
 
     <title>JanElim.kz</title>
     <meta name="description"
-          content="«Qpartners» - это уникальный медиа проект с широким набором возожностей для взаймодествия с участниками виртуального рынка"/>
+          content="«JanELim» - это уникальный медиа проект с широким набором возожностей для взаймодествия с участниками виртуального рынка"/>
     <meta name="keywords" content="Qpartners"/>
 
 @endsection
