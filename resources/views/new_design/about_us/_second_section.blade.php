4291dcb710c578e8927bea6240b65dc720eea44b
game-devel1 <rahatsadykov784@gmail.com>
2020-08-30 20:41:21 +0600

    about us text changed

--- resources/views/new_design/about_us/_second_section.blade.php
+++ resources/views/new_design/about_us/_second_section.blade.php
@@ -4,23 +4,39 @@
       <div class="row">
         <div class="col-xs-12">
           <div class="txt">
-            <h2>{{ $guide_text->title }}</h2>
-            <p>{{ $guide_text->text_body }}</p>            
+            {{-- <h2>{{ $guide_text->title }}</h2> --}}
+            {{-- <p>{{ $guide_text->text_body }}</p> --}}
+            <h2>Добро пожаловать в компанию Jan Elim!</h2>
+            <p>
+              Эликсир – это натуральные средства, которые помогают восстановлению и правильной работе организма человека.
+            </p>
+            <p>
+              Данная линейка продукции стимулируют правильную работу разных органов и систем жизнедеятельности человека (нервная система, иммунитет, система пищеварения, система кровеносных сосудов и т.д.).
+            </p>
+            <p>
+              В состав продукции, которые предлагает компания, входят только натуральные компоненты. В числе которых экстракты лечебных трав, медовая продукция и очищенная вода. Поэтому польза от продукции высокая, побочные эффекты отсутствуют.
+            </p>
+            <p>
+              Благодаря новым технологиям производства, готовая продукция сохраняет в себе до 99% полезных компонентов лечебных трав.
+            </p>
           </div>
           <p style="white-space: pre-line;">
-            <strong style="white-space: pre-line;">{{ $guide_text->author_full_name }}</strong>
-            {{ $guide_text->author_responsibility }}
+            <strong style="white-space: pre-line;">
+              В светлое будущее вместе с компанией Jan Elim!
+              {{-- {{ $guide_text->author_full_name }} --}}
+            </strong>
+            {{-- {{ $guide_text->author_responsibility }} --}}
           </p>
           <div class="mt-follow-holder">
             <span class="title">Follow Us</span>
             <!-- Social Network of the Page -->
             <ul class="list-unstyled social-network">
               <li><a href="{{ $guide_text->author_twitter_link }}"><i class="fa fa-twitter"></i></a></li>
-              <li><a href="{{ $guide_text->author_facebook_link }}"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="{{ $guide_text->author_twitter_link }}"><i class="fa fa-facebook"></i></a></li>
               {{-- <li><a href="#"><i class="fa fa-google-plus"></i></a></li> --}}
               {{-- <li><a href="#"><i class="fa fa-youtube"></i></a></li> --}}
               {{-- <li><a href="#"><i class="fa fa-linkedin"></i></a></li> --}}
-              <li><a href="{{ $guide_text->author_whatsapp_link }}"><i class="fa fa-whatsapp"></i></a></li>
+              <li><a href="{{ $guide_text->author_twitter_link }}"><i class="fa fa-whatsapp"></i></a></li>
             </ul>
             <!-- Social Network of the Page end -->
           </div>
