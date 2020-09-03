4573af940ee2c46d33799a0f9489bcd98437a182
AminDiyasSerikuly <amindiyass@gmail.com>
2020-09-03 19:31:34 +0600

    fix bug with adaptive design in index page

--- app/Http/Controllers/Admin/OperationController.php
+++ app/Http/Controllers/Admin/OperationController.php
@@ -37,7 +37,7 @@ class OperationController extends Controller
                 'user_operation.operation_comment',
                 'user_operation.operation_id',
                 'operation.operation_name_ru',
-                'operation.gv_balance',
+                'user_operation.gv_balance',
                 'fond.fond_name_ru',
                 'operation_type.operation_type_name_ru',
                 'recipient_user.name as recipient_name',
