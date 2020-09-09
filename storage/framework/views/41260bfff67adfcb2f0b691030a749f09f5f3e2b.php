c474773adac3d2590de35e21955addfe23ed2404
game-devel1 <rahatsadykov784@gmail.com>
2020-09-09 16:15:37 +0600

    banners img added

--- storage/framework/views/41260bfff67adfcb2f0b691030a749f09f5f3e2b.php
+++ storage/framework/views/41260bfff67adfcb2f0b691030a749f09f5f3e2b.php
@@ -6,67 +6,50 @@
         <div class="banner-box sixth">
             <!-- banner-17 white start here -->
             <div class="banner-17 white">
-                <img src="http://placehold.it/385x275" alt="image description">
+                <img src="/custom2/img/banners/4.png" alt="image description">
                 <div class="holder">
-                    <h4>SHOES</h4>
+                    <h2><strong>Super Elixir <br>Clean</strong></h2>                    
+                    <div class="price-tag">
+                        <span>
+                            это супер эффектиное средство, которая укрепляет весь организм.
+                        </span>
+                        
+                        
+                    </div>
                 </div>
-            </div><!-- banner-17 white end here -->
+            </div><!-- banner-17 white end here -->           
             <!-- banner-18 right start here -->
             <div class="banner-18 right">
-                <img src="http://placehold.it/385x275" alt="image description">
+                <img src="/custom2/img/banners/5.png" alt="image description">
                 <div class="holder">
-                    <h2><strong>EMMERSON <br>MOUNTAINEERING</strong></h2>
-                    <ul class="mt-stars">
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star-o"></i></li>
-                    </ul>
+                    <h2><strong>Super Elixir <br>Hepato</strong></h2>                    
                     <div class="price-tag">
-                        <span class="price-off">$ 129.00</span>
-                        <span class="price">$ 119.00</span>
-                        <a class="btn-shop" href="#">
-                            <span class="icon-handbag"></span>
-                            <span>ADD TO CARD</span>
-                        </a>
+                        <span>
+                            сильное регенеративное средство. Защищает и восстанавливает внутренную оболочку кровеносных сосудов.
+                        </span>
+                        
+                        
                     </div>
                 </div>
             </div><!-- banner-18 right end here -->
         </div><!-- banner-box sixth end here -->
         <!-- banner-19 start here -->
         <div class="banner-19">
-            <img src="http://placehold.it/792x493" alt="image description">
-            <div class="holder">
-                <div class="txt">
-                    <strong class="heading">Excepteur sint occaecat</strong>
-                    <h3><strong>WATCHES</strong> Counter stool</h3>
-                    <p>Consectetur adipisicing elit. Beatae accusamus, optio, repellendus inventore</p>
-                    <a href="#" class="now">SHOP NOW</a>
-                </div>
-            </div>
+            <img src="/custom2/img/banners/8.png" alt="image description">
+            
         </div><!-- banner-19 end here -->
     </div><!-- banner-box fourth end here -->
     <!-- banner-box fifth start here -->
     <div class="banner-box fifth hidden-md hidden-sm hidden-xs">
         <!-- banner-20 start here -->
         <div class="banner-20">
-            <img src="http://placehold.it/387x520" alt="image description">
-            <div class="holder">
-                <h2>SQUARE SIGNATURE</h2>
-                <p>The unmistakable intertwined CC that shook up Fashion, as signed by Coco herself</p>
-                <span class="price-off">$ 129.00</span>
-                <span class="price">$ 99.00</span>
-                <a href="#" class="card">ADD TO CARD</a>
-            </div>
+            <img src="/custom2/img/banners/7.png" alt="image description">
+            
         </div><!-- banner-20 end here -->
         <!-- banner-21 right start here -->
         <div class="banner-21 right">
-            <img src="http://placehold.it/387x248" alt="image description">
-            <div class="holder">
-                <strong class="title">ACCESSORIES</strong>
-                <h2><strong>Check Print</strong> <br>Scarf</h2>
-                <a href="#" class="view">VIEW COLLECTION</a>
-            </div>
+            <img src="/custom2/img/banners/6.png" alt="image description">
+            
         </div><!-- banner-21 right end here -->
     </div><!-- banner-box fifth end here -->
 </div><!-- banner-frame end here -->
