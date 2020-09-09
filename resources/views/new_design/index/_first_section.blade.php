35b4fdc83305fe31a3a6f1bd68bd0cc7091e12e2
game-devel1 <rahatsadykov784@gmail.com>
2020-09-09 14:52:46 +0600

    change slider img

--- resources/views/new_design/index/_first_section.blade.php
+++ resources/views/new_design/index/_first_section.blade.php
@@ -2,7 +2,7 @@
     <div class="banner-slider">
         <div class="holder">
             <div class="img">
-                <img src="/custom2/img/slider/4.png" alt="image description">
+                <img src="/custom2/img/slider/1.1.png" alt="image description">
             </div>
             {{-- <div class="caption">
                 <div class="c1">
@@ -16,7 +16,7 @@
         </div>
         <div class="holder">
             <div class="img">
-                <img src="/custom2/img/slider/5.png" alt="image description">
+                <img src="/custom2/img/slider/2.1png.png" alt="image description">
             </div>
             {{-- <div class="caption">
                 <div class="c1">
@@ -30,7 +30,7 @@
         </div>
         <div class="holder">
             <div class="img">
-                <img src="/custom2/img/slider/6.png" alt="image description">
+                <img src="/custom2/img/slider/3.1.png" alt="image description">
             </div>
             {{-- <div class="caption">
                 <div class="c1">
--- storage/framework/views/2fd822fe1bdd721720708206002fda4ea05810c7.php
+++ storage/framework/views/2fd822fe1bdd721720708206002fda4ea05810c7.php
@@ -2,19 +2,19 @@
     <div class="banner-slider">
         <div class="holder">
             <div class="img">
-                <img src="/custom2/img/slider/4.png" alt="image description">
+                <img src="/custom2/img/slider/1.1.png" alt="image description">
             </div>
             
         </div>
         <div class="holder">
             <div class="img">
-                <img src="/custom2/img/slider/5.png" alt="image description">
+                <img src="/custom2/img/slider/2.1png.png" alt="image description">
             </div>
             
         </div>
         <div class="holder">
             <div class="img">
-                <img src="/custom2/img/slider/6.png" alt="image description">
+                <img src="/custom2/img/slider/3.1.png" alt="image description">
             </div>
             
         </div>
