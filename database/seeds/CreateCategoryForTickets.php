0ff7f85c0cffe566af516c5af8133e35a6d2020e
AminDiyasSerikuly <amindiyass@gmail.com>
2020-12-20 19:55:02 +0600

    truncate category seeder before set

--- database/seeds/CreateCategoryForTickets.php
+++ database/seeds/CreateCategoryForTickets.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use  Illuminate\Database\Query\Builder;
+
 class CreateCategoryForTickets extends Seeder
 {
     /**
@@ -11,6 +12,8 @@ class CreateCategoryForTickets extends Seeder
      */
     public function run()
     {
+
+        DB::table('category2s')->truncate();
         \App\Models\Category2::create([
             'name' => 'Проблема с бонусами'
         ]);
